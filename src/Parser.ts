37b4890ab1a58a8ddcd41ce88dad93f290ef4df6
来一斤BUG <1045290202@qq.com>
2024-05-07 11:35:05 +0800

    右括号单独出现时需要抛出异常

--- src/Parser.ts
+++ src/Parser.ts
@@ -145,7 +145,7 @@ export default class Parser {
                 break;
             }
             case SeparatorType.RIGHT_BRACE: {
-                break;
+                throw new SyntaxError("Unexpected '}'");
             }
         }
     }
